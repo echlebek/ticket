0239d42673b8c889f279f08518b781bddf403835
Dimitri Tcaciuc <dtcaciuc@gmail.com>
2022-03-04 23:48:06 -0800

    Fix race condition reading submission time; test cancellation.
    Minor code cleanup and rename.

--- ticket_test.go
+++ ticket_test.go
@@ -8,29 +8,71 @@ import (
 	"github.com/echlebek/ticket"
 )
 
-type Job struct{}
+type Job struct {
+	Delay time.Duration
+}
 
-type JobHandler[Job any] struct {
+type JobHandler[J Job] struct {
 	Run bool
 }
 
-func (j *JobHandler[Job]) Handle(ctx context.Context, jobs []Job) error {
-	if len(jobs) > 0 {
-		j.Run = true
+func (h *JobHandler[J]) Handle(ctx context.Context, jobs []J) error {
+	if len(jobs) == 0 {
+		return nil
 	}
+
+	done := make(chan struct{})
+
+	go func() {
+		for _, j := range jobs {
+			time.Sleep(j.Delay)
+		}
+		done <- struct{}{}
+	}()
+
+	select {
+	case <- done:
+		h.Run = true
+	case <- ctx.Done():
+	}
+
 	return nil
 }
 
 func TestSmoke(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	handler := JobHandler[Job]{}
 	server := ticket.NewBatchServer[Job](ctx, 100, time.Millisecond, &handler)
+
 	stub := server.Accept(Job{})
 	if err := stub.Wait(context.Background()); err != nil {
 		t.Fatal(err)
 	}
+
 	if !handler.Run {
 		t.Error("handler not run")
+	}
+}
+
+
+func TestCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	handler := JobHandler[Job]{}
+	server := ticket.NewBatchServer[Job](ctx, 100, time.Millisecond, &handler)
+
+	stub := server.Accept(Job{Delay: 10*time.Second})
+
+	time.Sleep(200 * time.Millisecond)
+	cancel()
+
+	if err := stub.Wait(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+
+	if handler.Run {
+		t.Error("handler still ran")
 	}
 }
